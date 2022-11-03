b49f177f4c0d527a39cd564dbf9785f33ad3d531
Victoria <veerod0804@yahoo.com>
2022-11-03 16:35:14 -0500

    completed tags

--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -17,6 +17,9 @@ public class Tag extends AbstractEntity {
     @NotBlank
     private String name;
 
+    @ManyToMany(mappedBy = "tags")
+    private final List<Event> events = new ArrayList<> ();
+
     public Tag(String name) {
         this.name = name;
     }
@@ -35,4 +38,8 @@ public class Tag extends AbstractEntity {
         this.name = name;
     }
 
+    public final List<Event> getEvents() {
+
+        return events;
+    }
 }
